a300950b3c71176013ba11188d9ac7f13911d4e5
Adharsh269 <adharshbusa1@gmail.com>
2024-09-30 18:39:34 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import ejs from "ejs";
 
 const app = express();
 const port = 3000;
+app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
+
+const blog = [
+];
+function addBlog(title,description){
+    blog.push({title,description});
+}
 
 app.get("/" ,(req,res) => {
     res.render("index.ejs",{
@@ -17,24 +25,23 @@ app.get("/add",(req,res)=>{
 app.post("/add",(req,res)=>{
     const h=req.body.t;
     const c=req.body.b;
-    console.log(h);
-    console.log(c);
     if(!h || !c){
         return res.status(400).send("something is empty.")
     }
-    blog.push({title:h,description:c});
+    addBlog(h,c);
+    console.log(blog);
     res.redirect("/");
 })
+
+app.use((err, req, res, next) =>{
+    console.error(err.stack);
+    res.status(500).send("Something went wrong");
+});
 
 app.listen(port, ()=>{
     console.log("Listening on port",port);
 });
 
-const blog={
-    title:[
-
-    ],
-    description:[
-
-    ]
-};
+function removeBlog(){
+    blog.pop();
+}
